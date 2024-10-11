466caf74324d5b83f1e31d811d91b0eefdf0c6dd
IvanSemov44 <ivan.semov.44@gmail.com>
2024-10-11 19:07:18 +0300

    FinishWithBigForm

--- TheFirstOne/src/Components/BigForm/index.jsx
+++ TheFirstOne/src/Components/BigForm/index.jsx
@@ -10,14 +10,52 @@ export default function BigForm() {
   const [subject, setSubject] = useState({
     english: true,
     maths: false,
-    phisics: false,
+    physics: false,
   });
+  const [resume, setResume] = useState("");
+  const [url, setUrl] = useState();
+  const [selectedOption, setSelectedOption] = useState("");
+  const [about, setAbout] = useState("");
 
   const handleSubjectChange = (sub) => {
     setSubject((prev) => ({
       ...prev,
       [sub]: !prev[sub],
     }));
+  };
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+    console.log(
+      firstName,
+      lastName,
+      email,
+      contract,
+      gender,
+      selectedOption,
+      subject,
+      resume,
+      url,
+      about
+    );
+  };
+
+  const handleReset = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setContract("");
+    setGender("male");
+    setSubject({
+      english: true,
+      maths: false,
+      physics: false,
+    });
+    setResume("");
+    setUrl("");
+    setSelectedOption("");
+    setAbout("");
   };
 
   return (
@@ -97,12 +135,83 @@ export default function BigForm() {
           <input
             type="checkbox"
             name="lang"
-            id="english"
-            checked={subject.english === true}
-            onChange={(e)=>handleSubjectChange("maths")}
+            id="maths"
+            checked={subject.maths === true}
+            onChange={(e) => handleSubjectChange("maths")}
           />
           Maths
-          
+          <input
+            type="checkbox"
+            name="lang"
+            id="english"
+            checked={subject.english === true}
+            onChange={(e) => handleSubjectChange("english")}
+          />
+          English
+          <input
+            type="checkbox"
+            name="lang"
+            id="physics"
+            checked={subject.physics === true}
+            onChange={(e) => handleSubjectChange("physics")}
+          />
+          Physics
+          <label htmlFor="file">Upload Resume*</label>
+          <input
+            type="file"
+            name="file"
+            id="file"
+            onChange={(e) => setResume(e.target.files[0])}
+            placeholder="Enter Upload File"
+            required
+          />
+          <label htmlFor="url">Enter Url*</label>
+          <input
+            type="url"
+            name="url"
+            id="url"
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="Enter Url"
+            required
+          />
+          <label>Select Your Choice</label>
+          <select
+            name="select"
+            id="select"
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)}
+          >
+            <option value="" disabled defaultValue={selectedOption === ""}>
+              Select your Ans
+            </option>
+            <optgroup label="Beginers">
+              <option value="1">HTML</option>
+              <option value="2">CSS</option>
+              <option value="3">JavaScript</option>
+            </optgroup>
+            <optgroup label="Advance">
+              <option value="4">React</option>
+              <option value="5">Node</option>
+              <option value="6">Express</option>
+              <option value="7">MongoDb</option>
+            </optgroup>
+          </select>
+          <label htmlFor="About">About</label>
+          <textarea
+            name="about"
+            id="about"
+            cols="30"
+            rows="10"
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="About your self"
+            required
+          ></textarea>
+          <button type="reset" value="reset" onClick={() => handleReset()}>
+            Reset
+          </button>
+          <button type="submit" value="submit" onClick={(e) => handleSubmit(e)}>
+            Submit
+          </button>
         </form>
       </fieldset>
     </div>
