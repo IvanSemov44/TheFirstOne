1e20e3a25add4fc5b536474b2dc641b9756a43d1
IvanSemov44 <ivan.semov.44@gmail.com>
2024-10-12 17:40:47 +0300

    Added React Router and demo with it

--- TheFirstOne/src/App.jsx
+++ TheFirstOne/src/App.jsx
@@ -5,17 +5,47 @@ import Multiple from "./Components/Multiple";
 import MyForm from "./Components/MyForm";
 import Uncontrolled from "./Components/Uncontrolled";
 import BigForm from "./Components/BigForm";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About/Index";
+import Team from "./Components/Team";
+import Company from "./Components/Company";
+import Contact from "./Components/Contact";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <BigForm />
-      {/* <Uncontrolled />
+      {/* <BigForm />
+      <Uncontrolled />
       <MyForm />
       <Multiple />
       <NameForm />
       <Dropdown />
       <Checkbox /> */}
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}>
+            <Route path="team" element={<Team />} />
+            <Route path="company" element={<Company />} />
+          </Route>
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
